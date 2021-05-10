f67c1eb0809cb03dacfc6560eaef900ef67a4813
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2021-05-10 10:39:17 +0200

    Scaffolds engaged

---
+++ HolidayMaker/HolidayMakerBackEnd/Models/Database/Guest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HolidayMakerBackEnd.Models.Database
+{
+    public partial class Guest
+    {
+        public Guest()
+        {
+            Reservations = new HashSet<Reservation>();
+            Reviews = new HashSet<Review>();
+            SavedHotels = new HashSet<SavedHotel>();
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual ICollection<SavedHotel> SavedHotels { get; set; }
+    }
+}
---
+++ HolidayMaker/HolidayMakerBackEnd/Models/Database/ReservationsDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HolidayMakerBackEnd.Models.Database
+{
+    public partial class ReservationsDetail
+    {
+        public int Adults { get; set; }
+        public int? Children { get; set; }
+        public bool? ExtraBed { get; set; }
+        public string CustomerMessage { get; set; }
+        public int ReservationId { get; set; }
+        public string Type { get; set; }
+
+        public virtual Reservation Reservation { get; set; }
+        public virtual Accomodation TypeNavigation { get; set; }
+    }
+}

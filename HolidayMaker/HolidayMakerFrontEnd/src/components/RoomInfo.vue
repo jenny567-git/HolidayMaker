066136e92ed3aab7e5fafd906be5fbafd7d20627
Jenny Wong <jennywong567@gmail.com>
2021-05-10 13:58:18 +0200

    new branch from hotelview

---
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/RoomInfo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+
+
+export default{
+    components:{
+        
+    },
+    computed:{
+        foods(){
+            return 
+        }
+    }
+}
+
+</script>

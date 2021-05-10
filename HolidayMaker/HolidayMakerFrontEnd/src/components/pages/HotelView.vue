9104454e6e0537fdb81b28fb9b7fd09d90866d84
Andreas Linné <andreas_linne@hotmail.com>
2021-05-10 14:07:36 +0200

    Added bootstrap 4 and hotel navigation template

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/HotelView.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/HotelView.vue
@@ -1,6 +1,40 @@
 <template>
     <div>
         <h1>This is a hotel name {{hotel.name}}</h1>
+
+        <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+		</div>
+		<div class="col-md-8">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<ul class="nav nav-pills">
+				<li class="nav-item">
+					<a class="nav-link active" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Profile</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Messages</a>
+				</li>
+				<li class="nav-item dropdown ml-md-auto">
+					 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+						 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+						<div class="dropdown-divider">
+						</div> <a class="dropdown-item" href="#">Separated link</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
     </div>
 </template>
 
--- HolidayMaker/HolidayMakerFrontEnd/src/store/index.js
+++ HolidayMaker/HolidayMakerFrontEnd/src/store/index.js
@@ -7,11 +7,11 @@ const store = createStore({
         hotels: {
             1:{
                 name: 'Hotel 1',
-                description: 'Desc 1'
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dapibus lacus a diam rhoncus suscipit. Nulla facilisi. Maecenas non metus faucibus, feugiat lectus non, elementum urna. Morbi viverra gravida diam, et tincidunt felis laoreet vitae. Suspendisse vel metus non ex tempus tincidunt. Proin egestas sapien nisi, eu elementum est aliquet.'
             },
             2:{
                 name: 'Hotel 2',
-                description: 'Desc 2'
+                description: 'In varius, nisi quis blandit porta, dolor tortor aliquam odio, eget consectetur lectus leo a massa. Proin dignissim dignissim porttitor. Praesent sed risus id diam dapibus consectetur. Vivamus sollicitudin urna ut tincidunt varius. Morbi congue malesuada erat id luctus. Nunc.'
             },
         },
         user: {

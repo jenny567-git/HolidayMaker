7053901dec8894bcc39b88fab476d68345935a5e
Jenny Wong <jennywong567@gmail.com>
2021-05-11 10:21:00 +0200

    changed grid

--- HolidayMaker/HolidayMakerFrontEnd/src/components/RoomInfo.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/RoomInfo.vue
@@ -6,51 +6,68 @@
       <div class="col-md-6">
         <ul class="list-group list-group-flush">
           <li class="list-group-item">
-            <div class="col-md-6">
-              <Images/>
-            </div>
-            <div class="col-md-6">
-            <h3>(room type 1)</h3>
-            <p>Max Capacity: (room.maxCapatiy) 
-              <i class="fas fa-user-alt"></i>
-            </p>
-            <p>Rooms:
-            <input type="number" min="0" value="0" />
-            </p>
             <div class="row">
-                  <div class="col-md-6">
-                    <p>Price/unit:</p>
-                  </div>
-                  <div class="col-md-6">
-                      <p style="color: red">214 SEK</p>
-                  </div>
+              <div class="col">
+                <Images/>
+              </div>
+              <div class="col">
+                <h3>(room type 1)</h3>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <p>Max Capacity: (room.maxCapatiy) 
+                    <i class="fas fa-user-alt"></i>
+                  </p>
                 </div>
+                <div class="col-md-6">
+                  <p>Rooms: 
+                    <input type="number" min="0" value="0" />
+                </p>
                 </div>
+              </div>
+              <div class="row">
+                    <div class="col-md-6">
+                      <p>Price/unit:</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p style="color: red">214 SEK</p>
+                    </div>
+              </div>
+            </div>
           </li>
 
 
           <li class="list-group-item">
-            <div class="col-md-6">
-              <Images/>
-            </div>
-            <div class="col-md-6">
-            <h3>(room type 1)</h3>
-            <p>Max Capacity: (room.maxCapatiy) 
-              <i class="fas fa-user-alt"></i>
-            </p>
-            <p>Rooms:
-            <input type="number" min="0" value="0" />
-            </p>
             <div class="row">
-                  <div class="col-md-6">
-                    <p>Price/unit:</p>
-                  </div>
-                  <div class="col-md-6">
-                      <p style="color: red">214 SEK</p>
-                  </div>
+              <div class="col">
+                <Images/>
+              </div>
+              <div class="col">
+                <h3>(room type 1)</h3>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <p>Max Capacity: (room.maxCapatiy) 
+                    <i class="fas fa-user-alt"></i>
+                  </p>
                 </div>
+                <div class="col-md-6">
+                  <p>Rooms: 
+                    <input type="number" min="0" value="0" />
+                </p>
                 </div>
+              </div>
+              <div class="row">
+                    <div class="col-md-6">
+                      <p>Price/unit:</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p style="color: red">214 SEK</p>
+                    </div>
+              </div>
+            </div>
           </li>
+          
         </ul>
         <router-link to="/hotels/1/Photos">Hotel 1 Photos </router-link>
       </div>

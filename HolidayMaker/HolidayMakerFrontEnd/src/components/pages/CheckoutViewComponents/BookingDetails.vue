9f62eff2b5902acdbc756a67c587211a2363b09b
Andreas Linné <andreas_linne@hotmail.com>
2021-05-13 13:47:56 +0200

    HG-19 Refactored code into components

---
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CheckoutViewComponents/BookingDetails.vue
@@ -0,0 +1,46 @@
+<template >
+    <div>
+        <h4 class="mb-3">Booking details</h4>
+        <ul class="list-unstyled" id="OrderDetailsList">
+            <li class="list-item">
+                Rooms:
+            </li>
+            <li v-for="(room, index) in rooms" :key="index">
+                <span v-if="room.nrOfRooms > 0">
+                    {{room.type}} : {{room.nrOfRooms}}
+                </span> 
+            </li>
+            <li class="list-item">
+                Service type: {{serviceType}}
+            </li>
+            <li class="list-item">
+                Extra bed: {{extraBed}}
+            </li>
+            <li class="list-item">
+                Price: {{price}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    computed:{
+        rooms(){
+            return this.$store.state.bookingDetails.rooms;
+        },
+        serviceType(){
+            return this.$store.state.bookingDetails.serviceType;
+        },
+        extraBed(){
+            return this.$store.state.bookingDetails.extraBed ? 'Yes' : 'No';
+        },
+        price(){
+            return '$343';
+        }
+    }
+}
+</script>
+<style >
+    
+</style>

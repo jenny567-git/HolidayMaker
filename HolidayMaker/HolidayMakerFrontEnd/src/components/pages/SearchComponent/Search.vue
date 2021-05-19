fc50014f8e7025c36d6a9aea02bf6b31bb70b124
Jenny Wong <jennywong567@gmail.com>
2021-05-19 12:12:34 +0200

    HG-125, HG-126, HG-124, HG-102 fixed layout of search bar, fixed persistent dropdown

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/SearchComponent/Search.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/SearchComponent/Search.vue
@@ -1,85 +1,118 @@
 <template>
-  <div class="container">
+  <div class="container text-start">
     <div class="row">
-      <div class="col-md-6">
-        <div class="input-group">
-          <!-- <label for="exampleDataList" class="form-label">Datalist example</label> -->
-          <input
-            class="form-control"
-            list="datalistOptions"
-            id="exampleDataList"
-            placeholder="Type to search..."
-          />
-          <datalist id="datalistOptions">
-            <option value="San Francisco" />
-            <option value="New York" />
-            <option value="Seattle" />
-            <option value="Los Angeles" />
-            <option value="Chicago" />
-          </datalist>
-
-          <div class="input-group-append">
-            <!-- <button class="input-group-text"><i class="fa fa-search"></i></button> -->
-            <Button
-              type="button"
-              icon="pi pi-search"
-              class="p-button-rounded p-button-success p-d-none p-d-md-inline-flex"
-            />
-          </div>
-        </div>
+      <div class="col-md-4">
+        <label for="searchInput">Location </label>
+      </div>
+      <div class="col-md-4">
+        <label for="range">Dates</label>
+      </div>
+      <div class="col-md-4">
+        <label for="guest">Guests & rooms </label>
       </div>
     </div>
     <div class="row">
-      <p>Adults: <InputNumber v-model="value" showButtons :min="0" :max="100" value="2"/></p>
-    <!-- </div>
-    <div class="row"> -->
-      <p>Children <InputNumber v-model="value" showButtons :min="0" :max="100" value="0"/></p>
-    <!-- </div>
-    <div class="row"> -->
-      <p>Room <InputNumber v-model="value" showButtons :min="0" :max="100" value="1"/></p>
-      
-      <label for="range">Dates</label>
-      <Calendar id="range" v-model="dates" selectionMode="range" :manualInput="false" :minDate="minDate"/>
+      <div class="col-md-4">
+        <input
+          class="form-control"
+          list="datalistOptions"
+          id="searchInput"
+          placeholder="Type to search..."
+        />
+        <datalist id="datalistOptions">
+          <option value="San Francisco" />
+          <option value="New York" />
+          <option value="Seattle" />
+          <option value="Los Angeles" />
+          <option value="Chicago" />
+        </datalist>
+      </div>
+      <div class="col-md-4">
+        <div class="p-fluid p-grid p-formgrid">
+        <Calendar
+          id="range"
+          v-model="dates"
+          selectionMode="range"
+          :manualInput="false"
+          :minDate="minDate"
+        />
+      </div>
+      </div>
+      <div class="col-md-3">
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuClickableInside" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+            Guest, rooms
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <div class="container text-start">
+              <p>
+                Adults
+                <InputNumber
+                  v-model="value"
+                  showButtons
+                  :min="0"
+                  :max="100"
+                  value="2"
+                />
+              </p>
+              <p>
+                Children
+                <InputNumber
+                  v-model="value"
+                  showButtons
+                  :min="0"
+                  :max="100"
+                  value="0"
+                />
+              </p>
+              <p>
+                Room
+                <InputNumber
+                  v-model="value"
+                  showButtons
+                  :min="0"
+                  :max="100"
+                  value="1"
+                />
+              </p>
+            </div>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-1">
+        <Button type="button" icon="pi pi-search" class="p-button-rounded p-button-success p-d-none p-d-md-inline-flex " >  </Button>
+      </div>
     </div>
   </div>
-  <!-- input field -->
 
-  <!-- <AutoComplete v-model="selectedCity" :suggestions="filteredCities" @complete="searchCity($event)" field="label" optionGroupLabel="label" optionGroupChildren="items">
-                <template #optiongroup="slotProps">
-                    <div class="p-d-flex p-ai-center country-item">
-                        <img src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" width="18" />
-                        <div class="p-ml-2">{{slotProps.item.label}}</div>
-                    </div>
-                </template>
-            </AutoComplete> -->
 </template>
 
 <script>
 import Button from "primevue/Button";
-import InputNumber from 'primevue/inputnumber';
-import Calendar from 'primevue/calendar';
+import InputNumber from "primevue/inputnumber";
+import Calendar from "primevue/calendar";
 
 export default {
   components: {
     Button,
     InputNumber,
-    Calendar
+    Calendar,
   },
   created() {
-        let today = new Date();
-        let month = today.getMonth();
-        let year = today.getFullYear();
-        let prevMonth = (month === 0) ? 11 : month -1;
-        let prevYear = (prevMonth === 11) ? year - 1 : year;
-        let nextMonth = (month === 11) ? 0 : month + 1;
-        let nextYear = (nextMonth === 0) ? year + 1 : year;
-        this.minDate = today;
-    },
+    let today = new Date();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let prevMonth = month === 0 ? 11 : month - 1;
+    let prevYear = prevMonth === 11 ? year - 1 : year;
+    let nextMonth = month === 11 ? 0 : month + 1;
+    let nextYear = nextMonth === 0 ? year + 1 : year;
+    this.minDate = today;
+  },
   data() {
-        return {
-            dates: null,
-            minDate: null,
-        }
-    }
-};
+    return {
+      dates: null,
+      minDate: null,
+    };
+  },
+}
 </script>

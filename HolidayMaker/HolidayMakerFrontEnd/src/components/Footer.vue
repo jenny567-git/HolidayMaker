92e9133c51cf660a7897275b65adc2ea39068ec7
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2021-05-20 10:14:33 +0200

    removed a style

--- HolidayMaker/HolidayMakerFrontEnd/src/components/Footer.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/Footer.vue
@@ -9,7 +9,6 @@
     <title>Untitled</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>

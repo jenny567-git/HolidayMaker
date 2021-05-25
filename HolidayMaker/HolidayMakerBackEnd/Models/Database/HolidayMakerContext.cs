89cd5b8a50df66a3e7ef95be9ac187eecc867433
Jenny Wong <jennywong567@gmail.com>
2021-05-25 08:21:24 +0200

    new scaffolding of database

--- HolidayMaker/HolidayMakerBackEnd/Models/Database/HolidayMakerContext.cs
+++ HolidayMaker/HolidayMakerBackEnd/Models/Database/HolidayMakerContext.cs
@@ -35,7 +35,7 @@ namespace HolidayMakerBackEnd.Models.Database
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Database=HolidayMaker;Server=tcp:holidaymaker.database.windows.net,1433;Initial Catalog=HolidayMaker;Persist Security Info=False;User ID=Grupp1;Password=Viärbäst!;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:holidaymaker.database.windows.net,1433;Initial Catalog=HolidayMaker;Persist Security Info=False;User ID=Grupp1;Password=Viärbäst!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
@@ -142,6 +142,8 @@ namespace HolidayMakerBackEnd.Models.Database
                 entity.Property(e => e.Description)
                     .IsRequired()
                     .IsUnicode(false);
+
+                entity.Property(e => e.ExtraBedFee).HasColumnName("extraBedFee");
 
                 entity.Property(e => e.Name)
                     .IsRequired()
@@ -257,6 +259,8 @@ namespace HolidayMakerBackEnd.Models.Database
                 entity.Property(e => e.Id)
                     .ValueGeneratedOnAdd()
                     .HasColumnName("ID");
+
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
 
                 entity.Property(e => e.Description)
                     .IsRequired()
--- HolidayMaker/HolidayMakerBackEnd/Models/Database/ReservedRoom.cs
+++ HolidayMaker/HolidayMakerBackEnd/Models/Database/ReservedRoom.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -9,7 +8,6 @@ namespace HolidayMakerBackEnd.Models.Database
     public partial class ReservedRoom
     {
         public int ReservationId { get; set; }
-      
         public int RoomId { get; set; }
         public int BookedRooms { get; set; }
 

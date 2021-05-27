17396273ac73b6a0e089fbda7b82bf9a43bf77d2
Jenny Wong <jennywong567@gmail.com>
2021-05-27 15:29:02 +0200

    changed ratingslider in add review to primevue rating component

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/HotelViewComponents/ReviewComponents/AddReview.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/HotelViewComponents/ReviewComponents/AddReview.vue
@@ -1,91 +1,87 @@
 <template>
-    <div id="test2">
+  <div id="test2">
     <form @submit="onSubmit">
-      <div class="pt-5">
-        <RatingSlider v-model="rating" @clicked="onClickChild" />
+      <div class="row pt-5 pb-3">
+        <div class="col-md-6 text-end">Rating</div>
+        <div class="col-md-6 text-start">
+          <Rating v-model="rating" :cancel="false" />
         </div>
-        <div class="form-group">
+      </div>
+      <div class="form-group">
         <label for="exampleFormControlInput1">Your name</label>
-        <input v-model="name" type="text" name='name' class="form-control" />
+        <input v-model="name" type="text" name="name" class="form-control" />
       </div>
       <div class="form-group">
         <label for="exampleFormControlInput1">Your email</label>
-        <input v-model="email" type="email" name='email' class="form-control" />
+        <input v-model="email" type="email" name="email" class="form-control" />
       </div>
       <div class="form-group">
         <label for="exampleFormControlTextarea1">Your message</label>
-        <textarea v-model="message" name='message' class="form-control" rows="3"></textarea>
+        <textarea
+          v-model="message"
+          name="message"
+          class="form-control"
+          rows="3"
+        ></textarea>
       </div>
-      <!-- <input type="submit" value="Submit" class="btn btn-block"/> -->
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
-import RatingSlider from './RatingSlider.vue'
+import Rating from "primevue/rating";
 
-  export default{
-      components: {
-        RatingSlider
-    },
-    data(){
-      return{
-        //bind to model
-        name:'',
-        email:'',
-        message:'',
-        rating:''
-        // newReview: new Review()
+export default {
+  components: {
+    Rating,
+  },
+  data() {
+    return {
+      //bind to model
+      name: "",
+      email: "",
+      message: "",
+      rating: null,
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+
+      if (!this.message || !this.name || !this.email) {
+        alert("Please fill in all fields");
+        return;
       }
+      console.log("in submit - " + this.rating);
+      let newReview = {
+        message: this.message,
+        name: this.name,
+        email: this.email,
+        rating: this.rating,
+      };
+
+      console.log(newReview);
+
+      this.message = "";
+      this.name = "";
+      this.email = "";
+      this.rating = null;
     },
-    beforecreated(){
-      console.log('created');
-      this.onClickChild();
-    },
-    methods:{
-      
-      onClickChild(getValue){
-        this.rating = getValue
-        console.log(`onclickChild - ${this.rating}`)
-      },
-      onSubmit(e){
-        e.preventDefault()
-
-        if(!this.message || !this.name || !this.email){
-          alert('Please fill in all fields')
-          return
-        }
-        console.log(`in submit - ${this.rating}`)
-        let newReview = {
-            //id: 5,
-            message: this.message,
-            name: this.name,
-            email: this.email,
-            rating: `${this.rating}`
-        }
-
-        console.log(newReview)
-
-        this.message=''
-        this.name = ''
-        this.email = ''
-        this.rating=''
-      }
-    }
-  }
+  },
+};
 </script>
 
 
 <style scoped>
-  #test2{
-        border-radius: 30px;
-        background-color: rgba(107, 146, 83, 0.5);
-        padding-left: 25px;
-        padding-right: 25px;
-        padding-bottom: 40px;
-        margin-top: 20px;
-        color:white;
-        /* text-shadow: 2px 2px black; */
-    }
+#test2 {
+  border-radius: 30px;
+  background-color: rgba(107, 146, 83, 0.5);
+  padding-left: 25px;
+  padding-right: 25px;
+  padding-bottom: 40px;
+  margin-top: 20px;
+  color: white;
+  /* text-shadow: 2px 2px black; */
+}
 </style>

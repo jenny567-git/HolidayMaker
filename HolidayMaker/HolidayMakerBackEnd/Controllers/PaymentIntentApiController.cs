3afab3b7d5449a4dd95422afa2aa4fe773d3c923
Andreas Linné <andreas_linne@hotmail.com>
2021-05-31 12:12:25 +0200

    HG-56 Stripe payment can now be confirmed

---
+++ HolidayMaker/HolidayMakerBackEnd/Controllers/PaymentIntentApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Stripe;
+using Newtonsoft.Json;
+
+namespace HolidayMakerBackEnd.Controllers
+{
+    [Route("api/create-payment-intent")]
+    [ApiController]
+    public class PaymentIntentApiController : Controller
+    {
+        [HttpPost]
+        public ActionResult Create(PaymentIntentCreateRequest request)
+        {
+            var paymentIntents = new PaymentIntentService();
+            var paymentIntent = paymentIntents.Create(new PaymentIntentCreateOptions
+            {
+                Amount = CalculateOrderAmount(request.Items),
+                Currency = "usd",
+            });
+            return Json(new { clientSecret = paymentIntent.ClientSecret });
+        }
+        private int CalculateOrderAmount(Item[] items)
+        {
+            // Replace this constant with a calculation of the order's amount
+            // Calculate the order total on the server to prevent
+            // people from directly manipulating the amount on the client
+            return 1400;
+        }
+        public class Item
+        {
+            [JsonProperty("id")]
+            public string Id { get; set; }
+        }
+        public class PaymentIntentCreateRequest
+        {
+            [JsonProperty("items")]
+            public Item[] Items { get; set; }
+        }
+    }
+}

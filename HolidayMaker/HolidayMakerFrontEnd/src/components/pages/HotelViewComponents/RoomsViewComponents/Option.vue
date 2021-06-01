9f8545a1da537b470e1adbc3fdfae6a307e296fc
Jenny Wong <jennywong567@gmail.com>
2021-06-01 11:04:38 +0200

    changed option to primevue

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/HotelViewComponents/RoomsViewComponents/Option.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/HotelViewComponents/RoomsViewComponents/Option.vue
@@ -1,7 +1,32 @@
 <template>
   <div>
-    <h3>Options</h3>
-    <div class="row">
+    <h3>Extra bed</h3>
+    <div class="p-field-checkbox">
+          <Checkbox id="option" v-model="checked" :binary="true" />
+          <label for="option">{{ checked }}</label>
+        </div>
+
+
+
+    <!-- with card -->
+
+    <!-- <Card>
+      <template #title>
+        <h3>Extra bed</h3>
+      </template>
+      <template #content>
+        <div class="p-field-checkbox">
+          <Checkbox id="option" v-model="checked" :binary="true" />
+          <label for="option">{{ checked }}</label>
+        </div>
+      </template>
+    </Card> -->
+
+
+
+    <!-- with bootstrap -->
+
+    <!-- <div class="row">
       <div class="input-group mb-3">
         <div class="input-group-text">
           <input
@@ -18,13 +43,24 @@
           aria-label="Text input with checkbox"
         />
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
+import Card from "primevue/card";
+import Checkbox from "primevue/checkbox";
+
 export default {
-    
+  components: {
+    Checkbox,
+    Card
+  },
+  data() {
+    return {
+      checked: false,
+    };
+  },
 };
 </script>
 

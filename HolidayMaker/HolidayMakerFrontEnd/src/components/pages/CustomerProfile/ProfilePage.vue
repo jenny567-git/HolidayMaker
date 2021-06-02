0ecd725aaf1189a9072df9f9c15ac69b780c0fc3
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2021-06-02 16:06:49 +0200

    d

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/ProfilePage.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/ProfilePage.vue
@@ -1,5 +1,16 @@
 <template>
- 
+ <div class="profile-content">
+                    <div class="py-4">
+                    <h5 class="mb-3">Recent posts</h5>
+                    <div class="p-4 bg-light rounded shadow-sm">
+                        <p class="font-italic mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        <ul class="list-inline small text-muted mt-3 mb-0">
+                            <li class="list-inline-item"><i class="fa fa-comment-o mr-2"></i>12 Comments</li>
+                            <li class="list-inline-item"><i class="fa fa-heart-o mr-2"></i>200 Likes</li>
+                        </ul>
+                    </div>
+                </div>
+                </div>
 </template>
 
 <script>

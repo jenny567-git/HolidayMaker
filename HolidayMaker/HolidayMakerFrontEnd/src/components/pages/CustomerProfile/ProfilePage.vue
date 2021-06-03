a12b3de3a30b9abd1233e578c41bc472d8b70e9c
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2021-06-03 11:00:59 +0200

    added more flaire to profile

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/ProfilePage.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/ProfilePage.vue
@@ -18,9 +18,12 @@
   </div>
   <div class="p-4 bg-light rounded shadow-sm">
       <p class="font-italic mb-0">
-        Ukubhuka have now officially opened for buisseness! The most popular booking site in Africa. I hope you enjoy our service to ensure
-        the most comfortable stay in whatever hotel of your choice!
+        We have increase our choice of hotels such as The table bay in south africa cape town and Fairmont Nile City in egypt cairo! We hope
+        that our customers will concider visiting these fantastic places soon! 
       </p>
+      <br>
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.africansafarisnetwork.com%2Fwp-content%2Fuploads%2F2017%2F05%2Ftable-bay-hotel-1.png&f=1&nofb=1" height="210px" width="350" alt="">
+      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hotelsrepublic.com%2Fwp-content%2Fuploads%2F2016%2F02%2FFairmont-Nile-City-Cairo-Egypt.jpg&f=1&nofb=1" height="210px" alt="">
       <ul class="list-inline small text-muted mt-3 mb-0">
         <li class="list-inline-item">
           <i class="fa fa-comment-o mr-2"></i>36 Comments
@@ -30,10 +33,30 @@
         </li>
       </ul>
     </div>
+    <br>
+    <div class="p-4 bg-light rounded shadow-sm">
+      <p class="font-italic mb-0">
+        Ukubhuka have now officially opened for buisseness! The most popular booking site in Africa. I hope you enjoy our service to ensure
+        the most comfortable stay in whatever hotel of your choice!
+      </p>
+      <ul class="list-inline small text-muted mt-3 mb-0">
+        <li class="list-inline-item">
+          <i class="fa fa-comment-o mr-2"></i>46 Comments
+        </li>
+        <li class="list-inline-item">
+          <i class="fa fa-heart-o mr-2"></i>641 Likes
+        </li>
+      </ul>
+    </div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
+
+img{
+    padding-right: 6px;
+}
+
 </style>

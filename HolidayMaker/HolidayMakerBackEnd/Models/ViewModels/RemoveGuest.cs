b4ff70c2e8019ec9ad53b1ff605ae3e63b43a36b
Sebastian Larsson <seblarsson87@gmail.com>
2021-06-07 11:55:01 +0200

    started with fronten

---
+++ HolidayMaker/HolidayMakerBackEnd/Models/ViewModels/RemoveGuest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HolidayMakerBackEnd.Models.ViewModels
+{
+    public class RemoveGuest
+    {
+        public int Id { get; set; }
+    }
+}
--- HolidayMaker/HolidayMakerFrontEnd/src/components/Navigator.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/Navigator.vue
@@ -1,55 +1,83 @@
 <template>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <title>Simple Navbar with flexbox</title>
-        </head>
-            <header>
-                <img class="" src="/src/assets/Logo5_cropped.png" alt="" height="80">
-                <nav>
-                    <ul class="nav-area">
-                        <li><a href="#"><router-link to="/hotels/1">Hotel 1 | </router-link></a></li>
-                        <li><a href="#"><router-link to="/hotels/2">Hotel 2 | </router-link></a></li>
-                        <li><a href="#"><router-link to="/result">Search result view | </router-link></a></li>
-                        <li><a href="#"><router-link to="/addReview">Add review </router-link></a></li>
-                        <li><a href="#"><router-link to="/checkout">Checkout</router-link></a></li>
-                    </ul>
-                </nav>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <title>Simple Navbar with flexbox</title>
+    </head>
+    <header>
+      <img class="" src="/src/assets/Logo5_cropped.png" alt="" height="80" />
+      <nav>
+        <ul class="nav-area">
+          <li>
+            <a href="#"><router-link to="/hotels/1">Hotel 1 | </router-link></a>
+          </li>
+          <li>
+            <a href="#"><router-link to="/hotels/2">Hotel 2 | </router-link></a>
+          </li>
+          <li>
+            <a href="#"
+              ><router-link to="/result">Search result view | </router-link></a
+            >
+          </li>
+          <li>
+            <a href="#"
+              ><router-link to="/addReview">Add review </router-link></a
+            >
+          </li>
+          <li>
+            <a href="#"><router-link to="/checkout">Checkout</router-link></a>
+          </li>
+        </ul>
+      </nav>
 
+      <!-- Detta ska flyttas till ovanför ikonen till höger -->
+      <div v-if="loggedIn">
+        <ul class="nav-area">
+          <li>
+            <p>{{ user.email }}</p>
+          </li>
+        </ul>
+      </div>
+      <li class="nav-item dropdown">
+        <a
+          class="nav-link dropdown-toggle pi pi-user"
+          id="navbarDropdownMenuLink"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+          style="fontsize: 2rem"
+        >
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <div v-if="!loggedIn">
+            <li>
+              <a class="dropdown-item" href="#"
+                ><router-link to="/login">Log in</router-link></a
+              >
+            </li>
+          </div>
 
+          <div v-if="loggedIn">
+            <li>
+              <a class="dropdown-item" href="" @click="logout()">Log out</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"
+                ><router-link to="/profile">Profile</router-link>
+              </a>
+            </li>
+          </div>
 
-                <!-- Detta ska flyttas till ovanför ikonen till höger -->
-                <div v-if="loggedIn">
-                    <ul class="nav-area">
-                        <li ><p>{{user.email}}</p></li>
-                    </ul>
-                    
-                    
-                   
-                   
-                </div>
-                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle pi pi-user" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="fontSize: 2rem">
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                   
-                    <div v-if="!loggedIn">
-                    <li ><a class="dropdown-item" href="#"><router-link to="/login">Log in</router-link></a></li>
-                    </div>
-
-                    <div v-if="loggedIn">
-                   <li ><a class="dropdown-item" href="" @click="logout()">Log out</a></li>
-                   <li><a class="dropdown-item" href="#"><router-link to="/test">Profile</router-link>  </a></li>
-                    </div>
-                    
-                    <li><a class="dropdown-item" href="#"><router-link to="/registration">Register</router-link></a></li>
-                </ul>
-                </li>
-            </header>
-    </html>
+          <li>
+            <a class="dropdown-item" href="#"
+              ><router-link to="/registration">Register</router-link></a
+            >
+          </li>
+        </ul>
+      </li>
+    </header>
+  </html>
 </template>
-
-
 
 <script>
 export default {
@@ -57,88 +85,79 @@ export default {
   data() {
     return {
       userInfo: {},
-    };
+    }
   },
   computed: {
-    loggedIn(){
-            return this.$store.state.user.loggedIn
+    loggedIn() {
+      return this.$store.state.user.loggedIn
     },
     user() {
-      return this.$store.state.user;
+      return this.$store.state.user
     },
-    
   },
   created() {
-    this.$store.dispatch("getReservationById", this.$route.params.id);
+    this.$store.dispatch('getReservationById', this.$route.params.id)
   },
-  methods:{
-      logout(){
-            this.$store.dispatch('logout')
-            
-        }
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    },
   },
-  
-};
+}
 </script>
 
-
-
-
-
 <style scoped>
-
-
 * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 a {
-    text-decoration: none;
+  text-decoration: none;
 }
 
 header {
-    margin-top: -60px;
-    display: -webkit-flex;
-    display: -moz-flex;
-    display: -ms-flex;
-    display: -o-flex;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 12px 70px;
-    background:#202a38;
-    background: rgb(51, 57, 58);
+  margin-top: -60px;
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: -ms-flex;
+  display: -o-flex;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 70px;
+  background: #202a38;
+  background: rgb(51, 57, 58);
 }
 
 .nav-area {
-    list-style: none;
+  list-style: none;
 }
 
 .nav-area li {
-    display: inline-block;
-    padding: 0 15px;
+  display: inline-block;
+  padding: 0 15px;
 }
 
 .nav-area li a {
-    color: rgb(224, 224, 224);
+  color: rgb(224, 224, 224);
 }
 .nav-area li p {
-    color: rgb(224, 224, 224);
+  color: rgb(224, 224, 224);
 }
 
 .nav-area li a:hover {
-    color: #ffffff;
+  color: #ffffff;
 }
 
 .nav-item {
- list-style-type: none;
+  list-style-type: none;
 }
 
 .pi {
-    color: rgb(224, 224, 224) !important;
-    list-style-type: none;
+  color: rgb(224, 224, 224) !important;
+  list-style-type: none;
 }
 
 /* .btn-area {
@@ -151,7 +170,4 @@ header {
     border-radius: 5px;
     background: #c7b09e;
 }  */
-
-
 </style>
-

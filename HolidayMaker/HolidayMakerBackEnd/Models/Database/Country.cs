bd802c5bca78d4d1b4804d5b8c29e6e4676d013f
Jenny Wong <jennywong567@gmail.com>
2021-06-08 12:43:52 +0200

    new scaffolding after adding images to db

--- HolidayMaker/HolidayMakerBackEnd/Models/Database/Country.cs
+++ HolidayMaker/HolidayMakerBackEnd/Models/Database/Country.cs
@@ -10,13 +10,11 @@ namespace HolidayMakerBackEnd.Models.Database
         public Country()
         {
             Cities = new HashSet<City>();
-            Hotels = new HashSet<Hotel>();
         }
 
         public int Id { get; set; }
         public string CountryName { get; set; }
 
         public virtual ICollection<City> Cities { get; set; }
-        public virtual ICollection<Hotel> Hotels { get; set; }
     }
 }

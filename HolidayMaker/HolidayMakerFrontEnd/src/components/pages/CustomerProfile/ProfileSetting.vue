bc22890ec115ea7720c12c145df4562eb8d39b04
Sebastian Larsson <seblarsson87@gmail.com>
2021-06-08 14:23:49 +0200

    added prevent event

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/ProfileSetting.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/ProfileSetting.vue
@@ -1,5 +1,5 @@
 <template>
-<form class="form1" @submit="checkPasswords">
+<form class="form1" @submit.prevent="checkPasswords">
         <div class="card-body">
             <div class="row gutters">
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">

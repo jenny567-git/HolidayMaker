4573ef120a753cd5025d878fe9ff008aced6497f
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2021-06-08 23:48:29 +0200

    fgsdfs

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/Extra pages/TouristAttraction.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/Extra pages/TouristAttraction.vue
@@ -1,8 +1,8 @@
 <template>
   <Card id="card">
     <template #content>
-        
-						<h3 class="mb-30">Giraffes!</h3>
+
+						<!-- <h3 class="mb-30">Giraffes!</h3>
 						<div class="row">
 							<div class="col-md-3">
 								<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.ItwnPurO78V7BKktMf9nzAEsDG%26pid%3DApi&f=1" alt="" class="img-fluid">
@@ -27,9 +27,56 @@
 								<img src="https://i.insider.com/57ac8dbace38f234008b6153?width=1500" alt="" class="img-fluid">
 							</div>
 						</div>
-					</div>
+					</div> -->
 
                 <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card p-0">
+                            <div class="card-image"> <img src="https://images.pexels.com/photos/2746187/pexels-photo-2746187.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""> </div>
+                            <div class="card-content d-flex flex-column align-items-center">
+                                <h4 class="pt-2">SomeOne Famous</h4>
+                                <h5>Creative Desinger</h5>
+                                <ul class="social-icons d-flex justify-content-center">
+                                    <li style="--i:1"> <a href="#"> <span class="fab fa-facebook"></span> </a> </li>
+                                    <li style="--i:2"> <a href="#"> <span class="fab fa-twitter"></span> </a> </li>
+                                    <li style="--i:3"> <a href="#"> <span class="fab fa-instagram"></span> </a> </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card p-0">
+                            <div class="card-image"> <img src="https://images.pexels.com/photos/381843/pexels-photo-381843.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""> </div>
+                            <div class="card-content d-flex flex-column align-items-center">
+                                <h4 class="pt-2">SomeOne Famous</h4>
+                                <h5>Creative Desinger</h5>
+                                <ul class="social-icons d-flex justify-content-center">
+                                    <li style="--i:1"> <a href="#"> <span class="fab fa-facebook"></span> </a> </li>
+                                    <li style="--i:2"> <a href="#"> <span class="fab fa-twitter"></span> </a> </li>
+                                    <li style="--i:3"> <a href="#"> <span class="fab fa-instagram"></span> </a> </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card p-0">
+                            <div class="card-image"> <img src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""> </div>
+                            <div class="card-content d-flex flex-column align-items-center">
+                                <h4 class="pt-2">SomeOne Famous</h4>
+                                <h5>Creative Desinger</h5>
+                                <ul class="social-icons d-flex justify-content-center">
+                                    <li style="--i:1"> <a href="#"> <span class="fab fa-facebook"></span> </a> </li>
+                                    <li style="--i:2"> <a href="#"> <span class="fab fa-twitter"></span> </a> </li>
+                                    <li style="--i:3"> <a href="#"> <span class="fab fa-instagram"></span> </a> </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
                 <div class="row">
                     <div class="col-lg-4">
                         <div class="card p-0">
@@ -98,22 +145,8 @@ export default {
     margin-top: 30px;
 }
 
- @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
-
- * {
-     margin: 0;
-     padding: 0;
-     box-sizing: border-box;
-     font-family: 'Poppins', sans-serif
- }
-
- body {
-     min-height: 80vh;
-     background: linear-gradient(to bottom, #000428, #004683)
- }
-
  .container {
-     margin-top: 100px
+     margin-top: 20px
  }
 
  .container .row .col-lg-4 {

23f4bb94c9c7d343ee38219c4e164b8da0097ac2
Sebastian Larsson <seblarsson87@gmail.com>
2021-06-08 09:20:53 +0200

    first commit

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/ProfileSetting.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/ProfileSetting.vue
@@ -6,13 +6,13 @@
                 </div>
                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
-                        <label for="fullName">First Name</label>
-                        <input type="text" class="form-control" id="fullName" placeholder="Enter first name">
+                        <label for="fullName">Full Name</label>
+                        <input type="text" class="form-control" id="fullName" placeholder="Enter first name" v-model="guest.fullName">
                     </div>
                 </div>
                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
-                        <label for="eMail">Last Name</label>
+                        <label for="eMail">Email</label>
                         <input type="text" class="form-control" id="eMail" placeholder=" Enter last Name">
                     </div>
                 </div>
@@ -79,6 +79,63 @@
 </template>
 
 <script>
+
+export default {
+
+  components:{
+  },
+  data(){
+    return{
+      guest:{
+        Fullname: "",
+        Street: "",
+        Zipcode: "",
+        City:"",
+        Country:"",
+        Phone:"",
+        Email: "",
+        Password: "",
+        confirmPassword:""
+      }
+    }
+  },
+  async mounted(){
+
+  },
+  methods:{
+    checkPasswords(){
+        var password = document.querySelector('.password').value;
+        var confirmPassword = document.querySelector('.confirmPassword').value;
+        
+        if(confirmPassword !=password){
+          document.querySelector('.text').innerHTML = "Passwords dont match"
+          
+          
+        }else if(password ===confirmPassword){
+          document.querySelector('.text').innerHTML="";
+          this.addGuest()
+        }
+    },
+    
+    async addGuest(){
+      
+      await fetch('https://localhost:44356/api/Guest/addGuest', {
+        method:'Post',
+        headers:{
+          'Accept': 'application/json',
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({Fullname:this.guest.Fullname, Street:this.guest.Street, Zipcode:this.guest.Zipcode, City:this.guest.City, Country:this.guest.Country, Phone:this.guest.Phone, Email:this.guest.Email, Password:this.guest.Password}),
+        
+      })
+       this.resetForm()
+    },
+      resetForm(){
+        console.log("hej")
+        document.querySelector('.form1').reset()
+      }
+  }
+}
 </script>
 
 <style scoped>

c8d5f25f7ff3fa9979bf04c271256de5c4a325c3
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2021-06-09 16:08:00 +0200

    Added hardcoded pictures for gallery (Could not fix pic per hotel in time)

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/HotelViewComponents/RoomsViewComponents/PhotoGallery.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/HotelViewComponents/RoomsViewComponents/PhotoGallery.vue
@@ -1,25 +1,25 @@
 <template>
 <div class="container">
   <div class="row">
-    <a href="https://unsplash.it/1200/768.jpg?image=251" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
-      <img src="https://unsplash.it/600.jpg?image=251" class="img-fluid rounded">
+    <a href="https://www.businessdestinations.com/wp-content/uploads/2016/07/Hotel-ICON-1.jpg" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
+      <img src="https://www.businessdestinations.com/wp-content/uploads/2016/07/Hotel-ICON-1.jpg" class="img-fluid rounded">
     </a>
-    <a href="https://unsplash.it/1200/768.jpg?image=252" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
-      <img src="https://unsplash.it/600.jpg?image=252" class="img-fluid rounded">
+    <a href="http://cdn-cms0.hotelrunner.com/assets/photos/large/65e9fef6-1141-4e3c-bc9f-f0972b7033a2.jpg" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
+      <img src="http://cdn-cms0.hotelrunner.com/assets/photos/large/65e9fef6-1141-4e3c-bc9f-f0972b7033a2.jpg" class="img-fluid rounded">
     </a>
-    <a href="https://unsplash.it/1200/768.jpg?image=253" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
-      <img src="https://unsplash.it/600.jpg?image=253" class="img-fluid rounded">
+    <a href="https://i.insider.com/5c7d45f926289814bc42db4d?width=700" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
+      <img src="https://i.insider.com/5c7d45f926289814bc42db4d?width=700g" class="img-fluid rounded">
     </a>
   </div>
   <div class="row">
-    <a href="https://unsplash.it/1200/768.jpg?image=254" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
-      <img src="https://unsplash.it/600.jpg?image=254" class="img-fluid rounded">
+    <a href="https://thewinery.imgix.net/twh-wk-matsal-uppdukad-jan2020-1-1200x1200.jpg?auto=compress%2Cformat&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=634&q=80&w=633.6" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
+      <img src="https://thewinery.imgix.net/twh-wk-matsal-uppdukad-jan2020-1-1200x1200.jpg?auto=compress%2Cformat&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=634&q=80&w=633.6" class="img-fluid rounded">
     </a>
-    <a href="https://unsplash.it/1200/768.jpg?image=255" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
-      <img src="https://unsplash.it/600.jpg?image=255" class="img-fluid rounded">
+    <a href="https://i.pinimg.com/originals/35/11/e0/3511e0db609c0ec5df82c80b7ddd6a31.jpg" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
+      <img src="https://i.pinimg.com/originals/35/11/e0/3511e0db609c0ec5df82c80b7ddd6a31.jpg" class="img-fluid rounded">
     </a>
-    <a href="https://unsplash.it/1200/768.jpg?image=256" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
-      <img src="https://unsplash.it/600.jpg?image=256" class="img-fluid rounded">
+    <a href="https://thewinery.imgix.net/twh-wk-matsal-uppdukad-jan2020-3-1200x1200.jpg?auto=compress%2Cformat&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=634&q=80&w=633.6" data-toggle="lightbox" data-gallery="gallery" class="col-md-4">
+      <img src="https://thewinery.imgix.net/twh-wk-matsal-uppdukad-jan2020-3-1200x1200.jpg?auto=compress%2Cformat&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=634&q=80&w=633.6" class="img-fluid rounded">
     </a>
   </div>
 </div>

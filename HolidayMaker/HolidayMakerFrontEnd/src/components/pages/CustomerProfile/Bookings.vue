4a30b6fe58d4adda451db860ecab09899ac1852e
Jenny Wong <jennywong567@gmail.com>
2021-06-12 17:21:09 +0200

    added pagination to profile bookings

--- HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/Bookings.vue
+++ HolidayMaker/HolidayMakerFrontEnd/src/components/pages/CustomerProfile/Bookings.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        <div v-if="!bookedHotels.length && !errorText">
+        <!-- <div v-if="!bookedHotels.length && !errorText">
+        </div> -->
+        <div v-if="!bookedHotels.length">
             <ProgressSpinner />
-        </div>
-        <div v-if="errorText">
             <p>No bookings have been loaded. Try refreshing or create a booking for them to show up here!</p>
         </div>
-        <BookedHotel v-for="hotel in bookedHotels" :hotel="hotel" :key="hotel.reservationId"></BookedHotel>
+        <BookedHotel v-for="hotel in displayList" :hotel="hotel" :key="hotel.reservationId"></BookedHotel>
+
+        <Pagination v-model="page" :records="this.bookedHotels.length" :per-page="perPage" @paginate="myCallback"/>
+
         <ConfirmDialog></ConfirmDialog>
     </div>
 </template>
@@ -14,33 +17,50 @@
 import BookedHotel from './BookedHotel.vue'
 import ConfirmDialog from "primevue/confirmdialog";
 import ProgressSpinner from 'primevue/progressspinner';
+import Pagination from 'v-pagination-3';
+
 export default {
     data(){
         return{
-            errorText: false
+            errorText: false,
+            page: 1,
+            perPage: 5,
+            records:[]
         }
     },
     components:{
         BookedHotel,
+        Pagination,
         ConfirmDialog,
         ProgressSpinner
     },
-    async created(){
+    created(){
         // Get booked hotels
-        await this.$store.dispatch('getBookings');
-        
+        this.$store.dispatch('getBookings');
+
+        for (let i = 1; i <= this.bookedHotels.length; i++) {
+        this.records.push(`Item ${i}`);
+        }
+
         if(!this.bookedHotels.length){
             this.errorText = true;
         }
-        
     },
     computed:{
         bookedHotels(){
             return this.$store.state.bookedHotels;
+        },
+        displayList(){
+            this.records = this.$store.state.bookedHotels;
+            const startIndex = this.perPage * (this.page - 1);
+            const endIndex = startIndex + this.perPage;
+            return this.records.slice(startIndex, endIndex);
+        }
+    },
+    methods:{
+        myCallback: function(page) {
+        // no need for callback here since you have all your data loaded already
         }
     }
 }
 </script>
-<style lang=""> 
-    
-</style>
